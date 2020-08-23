a20671c1785643f9e01c5987727ab67fe9b4760a
Jennifer Schlierman <jd9913@gmail.com>
2020-08-23 13:58:59 -0700

    file structure changes

--- highScores.js
+++
@@ -1,8 +0,0 @@
-var highScoresList = document.getElementById('highScoresList');
-var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoresList.innerHTML = highScores
-    .map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`;//using map to take an array of items and convert to something else (an HTML string)
-    })
-    .join("");
